5e19544d48b262614984d19e64e86cfa1a5ffd9f
marc100s <marc@ellipsi.net>
2025-10-16 19:30:49 +0200

    feat: implement improvements

--- lib/inngest/client.ts
+++ lib/inngest/client.ts
@@ -2,5 +2,6 @@ import { Inngest } from "inngest";
 
 export const inngest = new Inngest({
   id: "signalist",
+  eventKey: process.env.INNGEST_EVENT_KEY,
   ai: { gemini: { api: process.env.GEMINI_API_KEY } },
 });
